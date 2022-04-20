9b73c476ca7fa1346b92c850aa39319e4209355c
ivandonatongua25 <101301671+ivandonatongua25@users.noreply.github.com>
2022-04-20 09:20:36 -0300

    vamos

--- app.js
+++ app.js
@@ -1,8 +1,49 @@
 const express = require("express");
 const app = express();
-app.get("/",(req,res)=> {
+const path = require("path");
+
+
+
+let producto ={
+    tipoProducto : null,
+    precio : null,
+    cantidad : null
+}
+
+
+
+/*app.get("/",(req,res)=> {
     res.send("Bienvenido A Mi Pagina");
 });
+app.get("/home",(req,res)=> {
+    res.send("Bienvenido Al home");
+});
+app.get("/perfil",(req,res) => {
+    res.send(saludo);
+});
+app.get("/producto/agregar",(req,res)=>{
+    res.send(producto);
+});
+*/
+
+app.get("/home",(req,res)=>{
+    let pathtml = path.join(__dirname,"./views/home.html");
+    res.sendFile(pathtml);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(1998,()=> {
     console.log("Levantó el Navegador");
 })
+
